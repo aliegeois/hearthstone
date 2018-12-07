7f8c7f4ef67dd7ff3e7e4cf22a63c13da815ef7e
Kairides <gueguen.ronan1@gmail.com>
2018-12-07 13:25:41 +0100

    Modification de la procedure cast()

--- server-java/src/game/CardSpell.java
+++ server-java/src/game/CardSpell.java
@@ -9,14 +9,24 @@ public class CardSpell extends Card {
 	Set<MultipleTargetEffect> multipleEffects;
 	Set<GlobalEffect> globalEffects;
 	
-	CardSpell(int id, Player owner, String name, int mana, Set<Effect> effects) {
+	CardSpell(int id, Player owner, String name, int mana, Set<SingleTargetEffect> single, Set<MultipleTargetEffect> multiple, Set<GlobalEffect> global) {
 		super(id, owner, name, mana);
-		this.effects = effects;
+		this.singleEffects = single;
+		this.multipleEffects = multiple;
+		this.globalEffects = global;
 	}
 	
-	/*void cast(CardMinion target) {
-		for(Effect e : effects) {
+	void cast(CardMinion target) {
+		for(SingleTargetEffect s : singleEffects) {
 			
 		}
-	}*/
+		
+		for(MultipleTargetEffect m : multipleEffects) {
+			
+		}
+		
+		for(GlobalEffect g : globalEffects) {
+			
+		}
+	}
 }
