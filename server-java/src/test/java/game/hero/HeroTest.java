150dfafab1101b6e0dfc71a51d2e67ca2bc77738
E16B498D <charles.bequet--ermoy@etu.univ-nantes.fr>
2018-12-21 09:55:17 +0100

    Pom, pom pom

---
+++ server-java/src/test/java/game/hero/HeroTest.java
@@ -0,0 +1,28 @@
+package game.hero;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HeroTest {
+
+	@Test
+	void special() {
+		
+	}
+	
+	@Test
+	void takeDamage() {
+		
+	}
+	
+	@Test
+	void heal() {
+		
+	}
+	
+	@Test
+	void boostHealth() {
+		
+	}
+}
