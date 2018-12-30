d143d84d76512342584213d6a5f66ce95019c077
Antoine <antoineg.work@gmail.com>
2018-12-30 16:36:15 +0100

    Implémentation des effets

---
+++ frontend/src/app/effect.service.ts
@@ -0,0 +1,264 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export abstract class EffectService {
+
+    card: CardSpell;
+
+    constructor(card: CardSpell) {
+        this.card = card;
+    }
+
+    play(e?: Entity):void { //The parameters e is optionnal
+
+    }
+}
+
+
+export abstract class GlobalEffect extends EffectService {
+
+    constructor(card: CardSpell) {
+        super(card);
+    }
+
+    play():void {
+
+    }
+}
+
+export abstract class MultipleTargetEffect extends EffectService {
+    ownBoard: boolean;
+    opponentBoard: boolean;
+    ownHero: boolean;
+    opponentHero: boolean;
+
+    constructor(card: CardSpell, ownBoard: boolean, opponentBoard: boolean, ownHero: boolean, opponentHero: boolean) {
+        super(card);
+        this.ownBoard = ownBoard;
+        this.opponentBoard = opponentBoard;
+        this.ownHero = ownHero;
+        this.opponentHero = opponentHero;
+    }
+
+    play():void {
+
+    }
+
+    getOwnBoard(): boolean {
+        return this.ownBoard;
+    }
+
+    getOpponentBoard(): boolean {
+        return this.opponentBoard;
+    }
+
+    getOwnHero():boolean {
+        return this.ownHero;
+    }
+
+    getOpponentHero(): boolean {
+        return this.opponentHero;
+    }
+}
+
+export abstract class SingleTargetEffect extends EffectService {
+
+    constructor(card: CardSpell) {
+        super(card);
+    }
+
+    play(e?: Entity): void {
+        
+    }
+}
+
+export class MultiTargetBuff extends MultipleTargetEffect {
+    life: number;
+    attack: number;
+
+    constructor(card: CardSpell, ownBoard: boolean, opponentBoard: boolean, ownHero: boolean, opponentHero: boolean, life: number, attack: number) {
+        super(card, ownBoard, opponentBoard, ownHero, opponentHero);
+        this.life = life;
+        this.attack = attack;
+    }
+
+    play(e?: Entity): void {
+        if (e != null) { //TODO : éviter cette horreur (ts n'accepte pas les duplications de fonctions)
+            throw new Error("Unsupported Operation Exception");
+        }
+       
+        if(this.getOwnBoard()) {
+            for (var minion of this.card.getOwner().getBoard().values()) {
+                minion.boostHealth(this.life);
+                minion.boostDamage(this.attack);
+            }
+        }
+        if (this.getOpponentBoard) {
+            for (var minion of this.card.getOwner().getOpponent().getBoard().values()) {
+                minion.boostHealth(this.life);
+                minion.boostDamage(this.attack);
+            }
+        }
+    }
+}
+
+
+export class MultiTargetDamage extends MultipleTargetEffect {
+    quantity: number;
+
+    constructor(card: CardSpell, ownBoard: boolean, opponentBoard: boolean, ownHero: boolean, opponentHero: boolean, quantity: number) {
+        super(card, ownBoard, opponentBoard, ownHero, opponentHero);
+        this.quantity = quantity;
+    }
+
+    play(e?: Entity): void {
+        if (e != null) { //TODO : éviter cette horreur (ts n'accepte pas les duplications de fonctions)
+            throw new Error("Unsupported Operation Exception");
+        }
+
+        if (this.getOwnBoard()) {
+            for (var minion of this.card.getOwner().getBoard().values()) {
+                minion.takeDamage(this.quantity);
+            }
+        }
+        if (this.getOpponentBoard) {
+            for (var minion of this.card.getOwner().getOpponent().getBoard().values()) {
+                minion.takeDamage(this.quantity);
+            }
+        }
+        if (this.getOwnHero()) {
+            this.card.getOwner().getHero().takeDamage(this.quantity);
+        }
+        if (this.getOpponentHero()) {
+            this.card.getOwner().getOpponent().getHero().takeDamage(this.quantity);
+        }
+    }
+}
+
+export class MultiTargetHeal extends MultipleTargetEffect {
+    amount: number;
+
+    constructor(card: CardSpell, ownBoard: boolean, opponentBoard: boolean, ownHero: boolean, opponentHero: boolean, amount: number) {
+        super(card, ownBoard, opponentBoard, ownHero, opponentHero);
+        this.amount = amount;
+    }
+
+    play(e?: Entity): void {
+        if (e != null) { //TODO : éviter cette horreur (ts n'accepte pas les duplications de fonctions)
+            throw new Error("Unsupported Operation Exception");
+        }
+
+        if (this.getOwnBoard()) {
+            for (var minion of this.card.getOwner().getBoard().values()) {
+                minion.heal(this.amount);
+            }
+        }
+        if (this.getOpponentBoard) {
+            for (var minion of this.card.getOwner().getOpponent().getBoard().values()) {
+                minion.heal(this.amount);
+            }
+        }
+        if (this.getOwnHero()) {
+            this.card.getOwner().getHero().heal(this.amount);
+        }
+        if (this.getOpponentHero()) {
+            this.card.getOwner().getOpponent().getHero().heal(this.amount);
+        }
+    }
+}
+
+
+export class SingleTargetBuff extends SingleTargetEffect {
+    life: number;
+    attack: number;
+
+    constructor(card: CardSpell, life: number, attack: number) {
+        super(card);
+        this.life = life;
+        this.attack = attack;
+    }
+
+    play(e?: Entity): void {
+        if (e == null) { //TODO : éviter cette horreur (ts n'accepte pas les duplications de fonctions)
+            throw new Error("Unsupported Operation Exception");
+        }
+        e.boostHealh(this.life);
+        e.boostDamage(this.attack);
+    }
+}
+
+export class SingleTargetDamage extends SingleTargetEffect {
+    damage: number;
+
+    constructor(card: CardSpell, damage: number) {
+        super(card);
+        this.damage = damage;
+    }
+
+    play(e?: Entity): void {
+        if (e == null) { //TODO : éviter cette horreur (ts n'accepte pas les duplications de fonctions)
+            throw new Error("Unsupported Operation Exception");
+        }
+        e.takeDamage(this.damage);
+    }
+}
+
+export class SingleTargetHeal extends SingleTargetEffect {
+    amount: number;
+
+    constructor(card: CardSpell, amount: number) {
+        super(card);
+        this.amount = amount;
+    }
+
+    play(e?: Entity): void {
+        if (e == null) { //TODO : éviter cette horreur (ts n'accepte pas les duplications de fonctions)
+            throw new Error("Unsupported Operation Exception");
+        }
+        e.heal(this.amount);
+    }
+}
+
+
+export class Transform extends SingleTargetEffect {
+    into: CardMinion;
+
+    constructor(card: CardSpell, into: CardMinion) {
+        super(card);
+        this.into = into;
+    }
+
+    play(e: CardMinion):void {
+        if (e == null) {
+            throw new Error("Unsupported Operation Exception");
+        }
+        e = this.into;
+    }
+}
+
+export class DrawRandom extends EffectService {
+    cardNumber: number;
+    
+    constructor(card: CardSpell, cardNumber: number) {
+        super(card);
+        this.cardNumber = cardNumber;
+    }
+
+    play(e?: Entity): void {
+        if (e != null) { //TODO : éviter cette horreur (ts n'accepte pas les duplications de fonctions)
+            throw new Error("Unsupported Operation Exception");
+        }
+        for (let i:number = 0; i < this.cardNumber; i++) {
+            this.card.getOwner().drawCard();
+        }    
+    }
+
+    /*play(): void {
+       for (let i:number = 0; i < this.cardNumber; i++) {
+            this.card.getOwner().drawCard();
+        } 
+    }*/
+}
