607d93b4de01431b11e95a66d3d967636f06c65f
Ronan GUEGUEN <E134932Q@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2019-01-09 11:29:02 +0100

    Wut ?

--- server-java/src/test/java/game/PlayerTest.java
+++ server-java/src/test/java/game/PlayerTest.java
@@ -2,6 +2,7 @@ package test.java.game;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,11 +14,19 @@ public class PlayerTest{
 
     Player player1 = new Player("Billy", "Warrior");
     Player player2 = new Player("Bob", "Mage");
-    CardMinion carte1 = new CardMinion(1, player1, "carte1", 1, 1, 1, new HashSet<String>(), new HashMap<String,Integer>());
+    Set<String> cap1 = new HashSet<String>();
+    Set<String> cap2 = new HashSet<String>();
+    
+    CardMinion carte1, carte2;
+
 
     @BeforeEach
     public void Init(){
 
+        cap1.add("ready");
+        cap2.add("provocation");
+        this.carte1 = new CardMinion(1, player1, "carte1", 1, 1, 1, cap1, new HashMap<String,Integer>());
+        this.carte2 = new CardMinion(2, player2, "carte2", 1, 1, 1, cap2, new HashMap<String,Integer>());
     }
 
     @Test
