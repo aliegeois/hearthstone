8af6d1546d725280d2ed9d226843d87b28ac17c6
Antoine <antoineg.work@gmail.com>
2019-01-09 14:47:14 +0100

    Ajout d'un générateur de partie de base pour tester le frontend

--- frontend/src/app/constantes.service.ts
+++ frontend/src/app/constantes.service.ts
@@ -7,6 +7,31 @@ import { Injectable } from '@angular/core';
 export class ConstantesService {
 
   static HEROMAXHEALTH = 30;
+  static NBPOSSIBLEHEROES = 3;
 
   constructor() { }
+
+  getAleatoireHero(): String {
+    const min = 1;
+    const max = ConstantesService.NBPOSSIBLEHEROES;
+
+    const nbChoosed = Math.floor((Math.random() * max) + min);
+    let typeChoosed: String;
+
+    switch (nbChoosed) {
+      case 1:
+        typeChoosed = 'paladin';
+        break;
+      case 2:
+        typeChoosed = 'warrior';
+        break;
+      case 3:
+        typeChoosed = 'mage';
+        break;
+      case 4:
+        typeChoosed = 'casParDefaut';
+        break;
+    }
+    return typeChoosed;
+  }
 }
--- frontend/src/app/game/game.component.ts
+++ frontend/src/app/game/game.component.ts
@@ -9,18 +9,30 @@ import { Player } from '../app.component';
 export class GameComponent implements OnInit {
 
   players: Map<String, Player>;
-  playing: String; //Actual player
-  constructor() { }
+  playing: String; // Actual player
+  nbTurns: number;
+
+
+  constructor(player1Name?: String, player1Type?: String, player2Name?: String, player2Type?: String) {
+    const player1: Player = new Player(player1Name, player1Type); // Quand on a pas ces paramètres, ça met juste à null
+    const player2: Player = new Player(player2Name, player2Type); // Quand on a pas ces paramètres, ça met juste à null
+
+    if (player1Name != null) {
+      this.players.set(player1Name, player1);
+    }
+    if (player2Name != null) {
+      this.players.set(player2Name, player2);
+    }
+
+    this.nbTurns = 0;
+  }
 
   ngOnInit() {
   }
 
 
   init_test(): GameComponent {
-    let player1: Player = new Player('Claudius', 'paladin');
-    let player2: Player = new Player('Claudius', 'warrior');
-
-    let game: Game = new GameComponent
+    return new GameComponent('Claudius', 'paladin', 'Nero', 'warrior');
   }
 
 }
