7150853ad772cb8277968255b4baf29428f6eb1f
Arthur LIEGEOIS <E152689R@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2019-01-11 11:44:18 +0100

    +readme

---
+++ ministone/README.md
@@ -0,0 +1,18 @@
+## Comment lancer le(s) serveur(s)
+
+### Compiler le serveur la première fois:
+mvn clean install
+### Les fois d'après:
+mvn clean install
+### Lancer le serveur:
+java -jar target/ministone-0.0.1-SNAPSHOT.jar
+
+### Installer les packages nodejs la première fois (dans /client):
+npm i
+### Lancer Angular:
+ng serve
+
+## Utilisation standard:
+- Lancer le serveur Spring
+- Lancer le serveur Angular
+- Accéder au serveur sur "http://localhost/4200"
--- ministone/client/src/app/app.component.ts
+++ ministone/client/src/app/app.component.ts
@@ -1,45 +1,41 @@
 import { Component, OnInit } from '@angular/core';
- import * as Stomp from 'stompjs';
- import * as SockJS from 'sockjs-client';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+	
+	private serverUrl = 'http://localhost:8080/ministone-socket';
+	private stompClient;
 
-  private serverUrl = 'http://localhost:8080/ministone-socket';
-  private title = 'WebSockets chat';
-  private stompClient;
+	constructor() {
+		this.initializeWebSocketConnection();
+	}
 
-  constructor() {
-     this.initializeWebSocketConnection();
-  }
+	ngOnInit(): void {
+		document.getElementById('button').addEventListener('click', () => {
+			this.sendMessage('testMessage');
+		});
+	}
 
-  ngOnInit(): void {
-    document.getElementById('button').addEventListener('click', () => {
-      this.sendMessage('testMessage');
-    });
-  }
+	initializeWebSocketConnection() {
+		const ws = new SockJS(this.serverUrl);
+		this.stompClient = Stomp.over(ws);
 
-  initializeWebSocketConnection() {
-    const ws = new SockJS(this.serverUrl);
-    this.stompClient = Stomp.over(ws);
+		this.stompClient.connect({}, frame => {
+			this.stompClient.subscribe('/chat', message => {
+				if (message.body) {
+					console.log('Message reçu du serveur : ' + message.body);
+				}
+			});
+		});
+	}
 
-    this.stompClient.connect({}, frame => {
-
-      this.stompClient.subscribe('/chat', message => {
-        if (message.body) {
-          console.log('Message reçu du serveur : ' + message.body);
-        }
-      });
-
-    });
-  }
-
-  sendMessage(message) {
-    this.stompClient.send('/app/send/message' , {}, message);
-  }
-
+	sendMessage(message) {
+		this.stompClient.send('/app/send/message' , {}, message);
+	}
 }
