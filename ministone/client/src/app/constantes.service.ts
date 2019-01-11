098bff9b27f5a9b34663a613b982cc150808303f
Antoine <antoineg.work@gmail.com>
2019-01-11 11:52:20 +0100

    Ajout partie logique typescript dans le dossier propre

--- ministone/client/src/app/constantes.service.ts
+++ ministone/client/src/app/constantes.service.ts
@@ -3,7 +3,35 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class ConstantesService {
 
+  static HEROMAXHEALTH = 30;
+  static NBPOSSIBLEHEROES = 3;
+
   constructor() { }
+
+  getAleatoireHero(): String {
+    const min = 1;
+    const max = ConstantesService.NBPOSSIBLEHEROES;
+
+    const nbChoosed = Math.floor((Math.random() * max) + min);
+    let typeChoosed: String;
+
+    switch (nbChoosed) {
+      case 1:
+        typeChoosed = 'paladin';
+        break;
+      case 2:
+        typeChoosed = 'warrior';
+        break;
+      case 3:
+        typeChoosed = 'mage';
+        break;
+      case 4:
+        typeChoosed = 'casParDefaut';
+        break;
+    }
+    return typeChoosed;
+  }
 }
