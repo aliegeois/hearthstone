7802b767f2c4a06d4edec0dd5729c8933a9d45f5
aliegeois <pelanman@gmail.com>
2019-01-12 16:54:57 +0100

    trucs inutiles

--- ministone/server/src/main/java/fr/ministone/controller/GameController.java
+++ ministone/server/src/main/java/fr/ministone/controller/GameController.java
@@ -7,7 +7,6 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
